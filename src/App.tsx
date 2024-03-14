4eceaf02748e831810a38fbecc7420ab99da31d7
PedroSales117 <pedrohenrysalles@outlook.com>
2024-03-13 22:45:34 -0300

    feat(app): update app page to implement routes for all pages and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AllRepos from './pages/AllReposPage';
+import ListRepos from './components/ListRepositories';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/backend" element={<ListRepos filter="backend" />} />
+        <Route path="/ai" element={<ListRepos filter="ai" />} />
+        <Route path="/all" element={<AllRepos />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
