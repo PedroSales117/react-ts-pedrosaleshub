51389468fcc70560b8181141c7bbdf059f39a032
PedroSales117 <pedrohenrysalles@outlook.com>
2024-03-14 00:13:09 -0300

    feat(allReposPage): - add main repo banner - add hub repo button redirect link

--- src/pages/AllReposPage.tsx
+++ src/pages/AllReposPage.tsx
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Repository } from '../interfaces/Repository.interface';
 import styles from '../styles/AllReposPage.module.css';
 
 const AllRepos = () => {
     const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [mainRepo, setMainRepo] = useState<Repository | undefined>(undefined);
+    const [hubRepo, setHubRepo] = useState<Repository | undefined>(undefined); // Adicionado para armazenar o hubRepo
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchRepos = async () => {
             try {
-                const response = await axios.get('https://api.github.com/users/PedroSales117/repos');
-                setRepositories(response.data);
+                const response: AxiosResponse<Repository[], any> = await axios.get('https://api.github.com/users/PedroSales117/repos');
+                const data = response.data;
+                const foundHubRepo = data.find(repo => repo.topics.includes('hub'));
+                const foundMainRepo = data.find(repo => repo.topics.includes('main'));
+                const projectRepos = data.filter(repo => !repo.topics.includes('main') && !repo.topics.includes('hub'));
+
+                setRepositories(projectRepos);
+                setMainRepo(foundMainRepo);
+                setHubRepo(foundHubRepo);
             } catch (error) {
                 console.error('Erro ao buscar repositórios:', error);
             }
         };
 
         fetchRepos();
-    });
+    }, []);
 
     return (
         <div className={styles.container}>
@@ -36,8 +45,18 @@ const AllRepos = () => {
                         {'Back End'}
                     </button>
                 </div>
-                <button onClick={() => navigate('/')} className={styles.homeButton}>Home</button>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    {hubRepo && (
+                        <a className={styles.linkButton} href={hubRepo.html_url} target="_blank" rel="noopener noreferrer">hub Repository</a>
+                    )}
+                    <button onClick={() => navigate('/')} className={styles.homeButton}>Home</button>
+                </div>
             </div>
+            {mainRepo && (
+                <div className={styles.mainRepoBanner}>
+                    <a className={styles.mainRepoBannerButton} href={mainRepo.html_url} target="_blank" rel="noopener noreferrer">{mainRepo.name}</a>
+                </div>
+            )}
             <div className={styles.grid}>
                 {repositories.map(repo => (
                     <div key={repo.id} className={styles.repoCard}>
